3323b7071806c646154738a9e4086663dbf0365d
red <red@redttg.com>
2024-10-18 22:49:09 +0300

    create rm_lines module

---
+++ rm_lines/__init__.py
@@ -0,0 +1,13 @@
+from io import BytesIO
+from .reader import read_tree
+from .inker import tree_to_svg
+
+
+def rm_bytes_to_svg(data: bytes):
+    tree = read_tree(BytesIO(data))
+    with BytesIO() as f:
+        tree_to_svg(tree, f)
+        return f.getvalue()
+
+
+__all__ = ['read_tree', 'tree_to_svg']
