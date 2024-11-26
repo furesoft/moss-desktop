121de400c10e3304cb7ebb2a5a3bef292c0e1b72
JustRedTTG <redtonehair@gmail.com>
2024-11-26 09:06:11 +0200

    parse favorites info

--- melora/melora-extensions/extensions/imhentai_gallery_downloader.py
+++ melora/melora-extensions/extensions/imhentai_gallery_downloader.py
@@ -1,11 +1,45 @@
 import json
 import os.path
+from functools import wraps
+from threading import Thread
+import re
 
 from requests import Session
 
 from gui.defaults import Defaults
+from gui.screens.name_field_screen import NameFieldScreen
 from melora.extension_base import ExtensionBase
 
+SITE_URL = 'https://imhentai.xxx'
+PROFILE_URL = f'{SITE_URL}/profile/'
+FAVORITES_URL = f'{SITE_URL}/user/fav_pags.php'
+
+
+def threaded(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        thread = Thread(target=fn, args=args, kwargs=kwargs, daemon=True)
+        thread.start()
+        return thread
+
+    return wrapper
+
+
+def extract_galleries(response_text):
+    pattern = r'/gallery/\d+/'
+    galleries = set(re.findall(pattern, response_text))
+    return tuple(f'{SITE_URL}{gallery}' for gallery in galleries)
+
+def extract_gallery_info(response_text):
+    ids_to_extract = ['gallery_title', 'load_id', 'load_dir', 'gallery_id']
+    extracted_info = {}
+
+    for input_id in ids_to_extract:
+        pattern = rf'<input[^>]*id="{input_id}"[^>]*value="([^"]+)"[^>]*>'
+        match = re.search(pattern, response_text)
+        extracted_info[input_id] = match.group(1) if match else None
+
+    return extracted_info
 
 class Extension(ExtensionBase):
     ID = 'imhentai_gallery_downloader'
@@ -33,8 +67,49 @@ class Extension(ExtensionBase):
             json.dump(self.config, f)
 
     def authenticate(self):
-        username = self.config.get('username')
-        password = self.config.get('password')
+        token = self.config.get('token')
 
+        if not token:
+            NameFieldScreen(self.gui, f"{self.SHORT} PHPSESSID", on_submit=self.set_session_id)
+            return False
+
+        self.session.cookies['PHPSESSID'] = token
+
+        if not self.check_auth():
+            del self.config['token']
+            return self.authenticate()
+
+        return True
+
+    def check_auth(self):
+        response = self.session.get(PROFILE_URL, allow_redirects=False)
+        return response.ok
+
+    def set_session_id(self, token: str):
+        self.config['token'] = token
+
+    def get_gallery_info(self, url: str):
+        response = self.session.get(url)
+
+        return {
+            **extract_gallery_info(response.text)
+        }
+
+    @staticmethod
+    def get_referer_headers(url):
+        return {
+            'Referer': url,
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+
+    def get_favorites(self):
+        response = self.session.get(FAVORITES_URL, headers=self.get_referer_headers(PROFILE_URL))
+        galleries = extract_galleries(response.text)
+        infos = [self.get_gallery_info(gallery) for gallery in galleries]
+        print(infos)
+
+    @threaded
     def download_gallery(self):
-        self.authenticate()
+        if not self.authenticate():
+            return
+        favorites = self.get_favorites()
