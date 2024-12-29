352c92c151bb349be0cbebb3a69f601ccbbc5c73
JustRedTTG <redtonehair@gmail.com>
2024-12-29 10:24:24 +0200

    add melora root tools extension

---
+++ melora/melora-extensions/extensions/root_tools.py
@@ -0,0 +1,16 @@
+from melora.extension_base import ExtensionBase
+from rm_api import NewDocuments
+
+
+class Extension(ExtensionBase):
+    ID = 'root_tools'
+    NAME = "Root Tools"
+    SHORT = 'RT'
+    AUTHOR = "RedTTG"
+    ACTIONS = {
+        "Reset root": "reset_root"
+    }
+
+    def reset_root(self):
+        self.api.reset_root()
+        self.api.spread_event(NewDocuments())
