4178ce9dfbd46f63292a7ee1af9f613d0e1b1039
JustRedTTG <redtonehair@gmail.com>
2025-01-07 20:10:02 +0200

    make logo auto update color

--- gui/screens/mixins/logo.py
+++ gui/screens/mixins/logo.py
@@ -3,8 +3,8 @@ from typing import TYPE_CHECKING
 import pygameextra as pe
 
 from gui import APP_NAME
-from gui.events import ResizeEvent
 from gui.defaults import Defaults
+from gui.events import ResizeEvent
 
 if TYPE_CHECKING:
     from gui.aspect_ratio import Ratios
@@ -23,7 +23,7 @@ class LogoMixin:
             self.initialize_logo_and_line()
 
     def initialize_logo_and_line(self):
-        self.logo = pe.Text(
+        self._logo = pe.Text(
             APP_NAME,
             Defaults.LOGO_FONT, self.ratios.loader_logo_text_size,
             pe.math.center(
@@ -38,10 +38,16 @@ class LogoMixin:
         )
         self.line_rect = pe.Rect(0, 0, self.ratios.loader_loading_bar_width,
                                  self.ratios.loader_loading_bar_height)
-        self.line_rect.midtop = self.logo.rect.midbottom
+        self.line_rect.midtop = self._logo.rect.midbottom
         self.line_rect.top += self.ratios.loader_loading_bar_padding
 
         self.big_line_rect = pe.Rect(0, 0, self.ratios.loader_loading_bar_big_width,
                                      self.ratios.loader_loading_bar_big_height)
-        self.big_line_rect.midtop = self.logo.rect.midbottom
+        self.big_line_rect.midtop = self._logo.rect.midbottom
         self.big_line_rect.top += self.ratios.loader_loading_bar_padding
+
+    @property
+    def logo(self):
+        if self._logo.color != Defaults.TEXT_COLOR[0] or self._logo.background != Defaults.TEXT_COLOR[1]:
+            self.initialize_logo_and_line()
+        return self._logo
