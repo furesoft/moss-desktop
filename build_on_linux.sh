ef9dfb0e901f50a8d105d7ebcdab936f0c589216
JustRedTTG <redtonehair@gmail.com>
2025-01-10 09:12:13 +0200

    include extism in build script

--- build_on_linux.sh
+++ build_on_linux.sh
@@ -1,4 +1,8 @@
-#/bin/bash
-python3 -m nuitka --mode=app --include-data-dir=assets=assets --include-data-files=LICENSE=LICENSE \
+#!/bin/bash
+EXTISM_DIR=$(python -c 'import os, extism_sys; print(os.path.dirname(extism_sys.__file__))')
+
+python3 -m nuitka --mode=app --include-data-dir=assets=assets \
+--include-data-files=LICENSE=LICENSE \
+--include-data-files=${EXTISM_DIR}/extism_sys.so=extism_sys/extism_sys.so \
 --assume-yes-for-downloads --output-dir=build --script-name=moss.py --linux-icon=assets/icons/moss.png \
 --deployment --disable-console
