a7f902b69161a343aa75dac8719c3d32af8ce93c
JustRedTTG <redtonehair@gmail.com>
2025-01-18 11:14:53 +0200

    fix preview handler

--- gui/preview_handler.py
+++ gui/preview_handler.py
@@ -149,7 +149,7 @@ class PreviewHandler:
             rm_bytes = get_file_contents(document.api, file_hash, binary=True)
             if not rm_bytes:
                 raise Exception('Page content unavailable to construct preview')
-            image = Notebook_rM_Lines_Renderer.generate_expanded_notebook_from_rm(document.metadata, rm_bytes,
+            image = Notebook_rM_Lines_Renderer.generate_expanded_notebook_from_rm(document, rm_bytes,
                                                                                   use_lock=cls.PYGAME_THREAD_LOCK).get_frame_from_initial(
                 0, 0)
             image.resize(Defaults.PREVIEW_SIZE)
