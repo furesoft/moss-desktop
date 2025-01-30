25ac50d51f33b8fe346151f55ba61f2e08ceadfa
JustRedTTG <redtonehair@gmail.com>
2025-01-30 18:39:03 +0200

    include pygameextra in build_on_linux.sh

--- build_on_linux.sh
+++ build_on_linux.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 EXTISM_DIR=$(python -c 'import os, extism_sys; print(os.path.dirname(extism_sys.__file__))')
+PYGAMEEXTRA_DIR=$(python -c 'import os, pygameextra; print(os.path.dirname(pygameextra.__file__))')
 
-python3 -m nuitka --mode=app --include-data-dir=assets=assets \
+python3 -m nuitka --mode=app \
+--include-data-dir=assets=assets \
+--include-data-dir=${PYGAMEEXTRA_DIR}/assets=pygameextra/assets \
 --include-data-files=LICENSE=LICENSE \
 --include-data-files=${EXTISM_DIR}/extism_sys.so=extism_sys/extism_sys.so \
 --assume-yes-for-downloads --output-dir=build --script-name=moss.py --linux-icon=assets/icons/moss.png \
