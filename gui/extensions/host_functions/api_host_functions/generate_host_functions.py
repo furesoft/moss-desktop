ec1cedf8ad3454bbc6ef2f94777200ac9d32eb78
JustRedTTG <redtonehair@gmail.com>
2025-02-20 22:10:09 +0200

    improve comments and docstrings for the complex wrappers in API host functions

--- gui/extensions/host_functions/api_host_functions/generate_host_functions.py
+++ gui/extensions/host_functions/api_host_functions/generate_host_functions.py
@@ -9,6 +9,11 @@ from .. import definitions as d
 
 
 def document_wrapper(func):
+    """
+    This wrapper is for documents. Takes in document_uuid.
+
+    api.documents[document_uuid] will be returned.
+    """
     func.__annotations__.pop('item')
     ref = func.__annotations__.pop('ref', None)
     func.__annotations__ = {'document_uuid': str, **func.__annotations__}
@@ -24,16 +29,23 @@ def document_wrapper(func):
 
 
 def document_sub_wrapper(sub_attribute: str):
+    """
+    This wrapper is for sub attributes of a document. Takes in document_uuid.
+
+    api.documents[document_uuid].sub_attribute will be returned.
+    :param sub_attribute: The sub attribute of the document that will be affected
+    """
+
     def wrapper(func):
         func.__annotations__.pop('item')
-        ref = func.__annotations__.pop('ref', None)
+        ref = func.__annotations__.pop('ref', None)  # The function might accept the reference to the document
         func.__annotations__ = {'document_uuid': str, **func.__annotations__}
 
         @wraps(func)
         def inner_wrapper(document_uuid: str, *args, **kwargs):
             document = d.api.documents[document_uuid]
             if ref:
-                kwargs['ref'] = document
+                kwargs['ref'] = document  # Pass the document to the function per request
             return func(getattr(document, sub_attribute), *args, **kwargs)
 
         return inner_wrapper
@@ -42,6 +54,11 @@ def document_sub_wrapper(sub_attribute: str):
 
 
 def collection_wrapper(func):
+    """
+    This wrapper is for collections. Takes in document_collection_uuid.
+
+    api.collections[document_collection_uuid] will be returned.
+    """
     func.__annotations__.pop('item')
     func.__annotations__ = {'document_collection_uuid': str, **func.__annotations__}
 
@@ -54,16 +71,23 @@ def collection_wrapper(func):
 
 
 def collection_sub_wrapper(sub_attribute: str):
+    """
+    This wrapper is for sub attributes of a collection. Takes in document_collection_uuid.
+
+    api.collections[document_collection_uuid].sub_attribute will be returned.
+    :param sub_attribute: The sub attribute of the document collection that will be affected
+    """
+
     def wrapper(func):
         func.__annotations__.pop('item')
-        ref = func.__annotations__.pop('ref', None)
+        ref = func.__annotations__.pop('ref', None)  # The function might accept the reference to the collection
         func.__annotations__ = {'document_collection_uuid': str, **func.__annotations__}
 
         @wraps(func)
         def inner_wrapper(document_collection_uuid: str, *args, **kwargs):
-            document_collection = d.api.collections[document_collection_uuid]
+            document_collection = d.api.document_collections[document_collection_uuid]
             if ref:
-                kwargs['ref'] = document_collection
+                kwargs['ref'] = document_collection  # Pass the document collection to the function per request
             return func(getattr(document_collection, sub_attribute), *args, **kwargs)
 
         return inner_wrapper
@@ -72,6 +96,11 @@ def collection_sub_wrapper(sub_attribute: str):
 
 
 def metadata_wrapper(func):
+    """
+    This wrapper is for standalone metadata data stored on the extension manager. Takes in metadata_id.
+
+    em.metadata_objects[metadata_id] will be returned.
+    """
     func.__annotations__.pop('item')
     func.__annotations__ = {'metadata_id': str, **func.__annotations__}
 
@@ -84,22 +113,27 @@ def metadata_wrapper(func):
 
 
 def content_wrapper(func):
+    """
+    This wrapper is for standalone content data stored on the extension manager. Takes in content_id.
+    em.content_objects[content_id] will be returned
+    """
     func.__annotations__.pop('item')
     func.__annotations__ = {'content_id': str, **func.__annotations__}
 
     @wraps(func)
     def wrapper(content_id: str, *args, **kwargs):
-        content = d.extension_manager.metadata_objects[content_id]
+        content = d.extension_manager.content_objects[content_id]
         return func(content, *args, **kwargs)
 
     return wrapper
 
 
 def document_ref_wrapper(use_ref: bool = True):
+    # Create a basic wrapper which will add document_uuid to the metadata and content
     def wrapper(func):
         @wraps(func)
-        def wrapped(item: Any, *args, **kwargs):
-            ref = kwargs.pop('ref')
+        def wrapped(item: Any, *args, ref, **kwargs):
+            # Accepts the document through ref, cause item might be the metadata or content
             document_dict = func(item, *args, **kwargs)
             document_dict['metadata']['document_uuid'] = ref.uuid
             document_dict['content']['document_uuid'] = ref.uuid
@@ -107,7 +141,7 @@ def document_ref_wrapper(use_ref: bool = True):
 
         return wrapped
 
-    if use_ref:
+    if use_ref:  # By default, pass the item as the ref, cause the item would be the document
         def ref_wrapper(func):
             wrapped = wrapper(func)
 
@@ -118,8 +152,7 @@ def document_ref_wrapper(use_ref: bool = True):
             return ref_wrapped
 
         return ref_wrapper
-    else:
-        wrapper.__annotations__['ref'] = True
+    # If the item is not the document, then the ref should be passed from other wrappers
 
     return wrapper
 
@@ -129,6 +162,15 @@ def check_is_dict(_t: Type[TypedDict]):
 
 
 def generate_for_type(t: Type[TypedDict], item_type: type, prefix: str, wrapper, extra_item_data_wrapper=lambda x: x):
+    """
+    Helper function to generate host functions for a TypedDict type.
+
+    :param t: The TypedDict type
+    :param item_type: The type of the item that the TypedDict represents, e.g. Document, Metadata, Content
+    :param prefix: The prefix for the host functions
+    :param wrapper: A base wrapper to update how the item will be passed to the host functions
+    :param extra_item_data_wrapper: A wrapper to update the return value of the host functions to add extra data
+    """
     can_get = {}
     can_set = {}
     for name, _t in t.__annotations__.items():
@@ -192,10 +234,12 @@ generate_for_type(et.TRM_Document, Document, "moss_api_document_", document_wrap
 generate_for_type(et.TRM_DocumentCollection, DocumentCollection, "moss_api_collection_", collection_wrapper)
 
 # Metadata
-generate_for_type(et.TRM_MetadataDocument, Metadata, "moss_api_document_metadata_", document_sub_wrapper('metadata'),
-                  document_ref_wrapper(False))
-generate_for_type(et.TRM_MetadataBase, Metadata, "moss_api_collection_metadata_", collection_sub_wrapper('metadata'))
-generate_for_type(et.TRM_MetadataDocument, Metadata, "moss_api_metadata_", metadata_wrapper)
+generate_for_type(et.TRM_MetadataDocument, Metadata,  # Metadata of a document
+                  "moss_api_document_metadata_", document_sub_wrapper('metadata'), document_ref_wrapper(False))
+generate_for_type(et.TRM_MetadataBase, Metadata,  # Metadata of a collection
+                  "moss_api_collection_metadata_", collection_sub_wrapper('metadata'))
+generate_for_type(et.TRM_MetadataDocument, Metadata,  # Standalone metadata
+                  "moss_api_metadata_", metadata_wrapper)
 
 # Content
 generate_for_type(et.TRM_Content, Content, "moss_api_document_content_", document_sub_wrapper('content'))
