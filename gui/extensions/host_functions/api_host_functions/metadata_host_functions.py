c6692a6f0d144c95bd507f30181ca84808eec300
JustRedTTG <redtonehair@gmail.com>
2025-03-03 15:14:19 +0200

    Add new metadata host function

--- gui/extensions/host_functions/api_host_functions/metadata_host_functions.py
+++ gui/extensions/host_functions/api_host_functions/metadata_host_functions.py
@@ -1,7 +1,17 @@
-# from .export_types import TRM_MetadataDocument
-# from .. import definitions as d
-#
-#
-# @d.host_fn()
-# def moss_api_metadata_new() -> TRM_MetadataDocument:
-#     ...
+from typing import Annotated
+
+from box import Box
+from extism import Json
+
+from .import_types import MetadataNew
+from .. import definitions as d
+
+
+@d.host_fn()
+def moss_api_metadata_new(value: Annotated[MetadataNew, Json]) -> int:
+    _ = Box(value)
+    metadata = d.api.metadata.new(
+        _.name, _.parent, _.document_type
+    )
+    d.extension_manager.metadata_objects[id(metadata)] = metadata
+    return id(metadata)
