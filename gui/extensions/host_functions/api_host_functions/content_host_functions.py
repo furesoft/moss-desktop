ba06b68c7f7a6ac045fef97e717c8d291883f0fa
JustRedTTG <redtonehair@gmail.com>
2025-03-03 16:56:30 +0200

    Add content host functions

--- gui/extensions/host_functions/api_host_functions/content_host_functions.py
+++ gui/extensions/host_functions/api_host_functions/content_host_functions.py
@@ -1,16 +1,29 @@
-# from .. import definitions as d
-#
-#
-# @d.host_fn()
-# def moss_api_content_new_notebook():
-#     ...
-#
-#
-# @d.host_fn()
-# def moss_api_content_new_pdf():
-#     ...
-#
-#
-# @d.host_fn()
-# def moss_api_content_new_epub():
-#     ...
+from rm_api import Content
+from .. import definitions as d
+
+
+@d.host_fn()
+def moss_api_content_new_notebook(page_count: int = 1):
+    content = Content.new_notebook(d.api.author_id, page_count)
+
+    d.extension_manager.content_objects[id(content)] = content
+
+    return id(content)
+
+
+@d.host_fn()
+def moss_api_content_new_pdf():
+    content = Content.new_pdf()
+
+    d.extension_manager.content_objects[id(content)] = content
+
+    return id(content)
+
+
+@d.host_fn()
+def moss_api_content_new_epub():
+    content = Content.new_epub()
+
+    d.extension_manager.content_objects[id(content)] = content
+
+    return id(content)
