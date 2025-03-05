e2129b9b31485e3b4ce30e8c47dd79ae768f45d8
JustRedTTG <redtonehair@gmail.com>
2025-03-05 17:38:51 +0200

    Add MossFatal

--- gui/events.py
+++ gui/events.py
@@ -1,3 +1,13 @@
+from rm_api.notifications.models import Notification
+
+
 class ResizeEvent:
     def __init__(self, new_size):
         self.new_size = new_size
+
+
+class MossFatal(Notification):
+    """
+        This signals the code should stop execution instantly
+    """
+    ...
